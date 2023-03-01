45b24d47c5c5c1ca6a75fe407a319d8bdd1ecaf0
Oleg Trishchuk <oletri@auditdata.com>
2023-03-01 14:33:10 +0200

    Improved Zoom feature; Small fixes
    On ZoomLevel change, make a screenshot of whole Desktop and set as main
    window background (simulate magnifier). Zoom x1 clears the background to
    see live desktop. Cltr+drag drags background image.
    
    Small fixes:
     - added minimize hotkey (M);
     - added maximize hotkey (F);
     - added maximize on double-click;
     - added double-click on Contrast slider to reset contrast.

---
+++ PixelMatcher/Helpers/ScreenHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace PixelMatcher.Helpers
+{
+    internal static class ScreenHelper
+    {
+        public static BitmapImage GetScreenshot()
+        {
+            int screenLeft = SystemInformation.VirtualScreen.Left;
+            int screenTop = SystemInformation.VirtualScreen.Top;
+            int screenWidth = SystemInformation.VirtualScreen.Width;
+            int screenHeight = SystemInformation.VirtualScreen.Height;
+
+            using (var bitmap = new Bitmap(screenWidth, screenHeight))
+            {
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap.Size);
+                }
+                return ImageHelper.Convert(bitmap);
+            }
+        }
+    }
+}
